fe5450fa306e36d2f3a202dd15b04bd2aba9239a
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-09 13:41:14 +0530

    added login page  also

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,89 @@
 import React from "react";
+import styled from "styled-components";
+import { BackgroundImage, Header } from "../components";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Wrapper>
+      <BackgroundImage />
+      <div className="loginContent">
+        <Header />
+        {/* Form */}
+        <div className="form-Wrapper">
+          <div className="form">
+            <div className="title">
+              <h1>login</h1>
+            </div>
+            <div className="container">
+              <input type="email" placeholder="email" />
+              <input type="password" placeholder="password" />
+              <button>login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  .loginContent {
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: white;
+    background-color: rgba(0, 0, 0, 0.79);
+    width: 100vw;
+    height: 100vh;
+    grid-template-columns: 15vh 85vh;
+
+    .form-Wrapper {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+      height: 85vh;
+    }
+
+    .form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 2rem;
+      background-color: #000000b0;
+      height: 70vh;
+      padding: 2rem;
+      color: white;
+      border-radius: 0.4rem;
+    }
+
+    .container {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+      input {
+        border-radius: 0.4rem;
+        padding: 0.5rem 1rem;
+        width: 25rem;
+        height: 3.4rem;
+        outline: none;
+      }
+      button {
+        padding: 0.5rem;
+        background-color: red;
+        border: none;
+        cursor: pointer;
+        border-radius: 0.4rem;
+        height: 3.4rem;
+        color: white;
+        font-weight: bolder;
+        font-size: 1.05rem;
+      }
+    }
+  }
+`;
 
 export default LoginPage;
